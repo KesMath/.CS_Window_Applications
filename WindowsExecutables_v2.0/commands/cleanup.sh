1aa836d1c4a073a3bf3d26e3e8bb78c3ada28f78
unknown <kesmathieu30@gmail.com>
2020-08-27 19:43:10 -0400

    adding useful bash commands for debugging purposes

---
+++ WindowsExecutables_v2.0/commands/cleanup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -F '.\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ShutDown.exe'
+rm -F '.\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Shutdown.lnk'
---
+++ WindowsExecutables_v2.0/commands/go_to.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#non-executable in current directory... to be used when opening cmd prompt only!
+#cd '.\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup'
