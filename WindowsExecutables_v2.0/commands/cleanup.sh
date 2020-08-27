651ffddf42f54df36ae1c50792e161efcbff6d2f
unknown <kesmathieu30@gmail.com>
2020-08-27 19:44:19 -0400

    using proper arg for rm function

--- WindowsExecutables_v2.0/commands/cleanup.sh
+++ WindowsExecutables_v2.0/commands/cleanup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm -F '.\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ShutDown.exe'
-rm -F '.\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Shutdown.lnk'
+rm -f '.\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ShutDown.exe'
+rm -f '.\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Shutdown.lnk'
